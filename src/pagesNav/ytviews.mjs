40e0c2e0d52c6eabf2d439ceb19f5add417a5453
gemanepa <gemanepa@gmail.com>
2019-11-13 13:40:19 -0300

    Removed commented unused code

--- src/pagesNav/ytviews.mjs
+++ src/pagesNav/ytviews.mjs
@@ -4,6 +4,4 @@ export default async function navigatingYtviews(page) {
     consoleMessage('header', 'NAVIGATING YOUTUBE VIEWS', true)
     await page.goto('https://kingdomlikes.com/free_points/youtube-views');
     consoleMessage('info', 'Entered kingdomlikes youtubeviews page')
-    // await page.waitForNavigation({ waitUntil: ["networkidle0", "domcontentloaded"] });
-    // consoleMessage('info', 'Waiting for DOM Loading finished')
   }
